22d207d74580b2bb9eebb4cce7062c6afe258472
mjr2595 <michaeljohnraymond@gmail.com>
2024-01-24 00:17:42 -0600

    Add kDebugMode check for hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:pixel_adventure/pixel_adventure.dart';
@@ -7,7 +8,10 @@ import 'package:pixel_adventure/pixel_adventure.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   Flame.device.fullScreen();
+  Flame.device.setLandscape();
 
   PixelAdventure game = PixelAdventure();
-  runApp(GameWidget(game: game));
+  runApp(
+    GameWidget(game: kDebugMode ? PixelAdventure() : game),
+  ); // temporary workaround for hot reload
 }
